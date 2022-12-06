d68432577aa4800f8a16244006f1586cac1b17f8
Manoj <84076478+Manoj546@users.noreply.github.com>
2022-12-07 01:44:35 +0530

    upload

---
+++ userlogin-master/authentication/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views 
+
+app_name = "fileapp"
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('upload', views.upload, name='upload'),
+    path('uploads', views.send_files, name='uploads'),
+    path('signup', views.signup, name='signup'),
+    path('activate/<uid64>/<token>', views.activate, name='activate'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+]
---
+++ userlogin-master/userlogin/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'rakesh.cs20@bmsce.ac.in'
+EMAIL_HOST_PASSWORD = 'Raki@1509'
+EMAIL_PORT = 587
